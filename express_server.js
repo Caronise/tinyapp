dacfb0532a233556c1a6a1e41deff9ecb0e48cf6
Caronise <lucas.betancourt@hotmail.com>
2019-10-30 23:40:37 +0000

    Added login verification in order to access the 'Create NEW URL' page

--- express_server.js
+++ express_server.js
@@ -81,10 +81,17 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 app.get('/urls/new', (req, res) => {
-  let templateVars = {
-    user: users[req.cookies["user_id"]],
-  };
-  res.render("urls_new", templateVars);
+  if (req.cookies["user_id"] !== undefined) {
+    let templateVars = {
+      user: users[req.cookies["user_id"]],
+    };
+    res.render("urls_new", templateVars);
+  } else {
+    res.redirect("/login");
+  }
+
+ 
+  
 });
 
 app.get('/register', (req, res) => {
