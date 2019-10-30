ba396d9d778f5acd00af1c517de82995345a760a
Caronise <lucas.betancourt@hotmail.com>
2019-10-30 23:11:10 +0000

    Created registration and login routes, and also verification email and password verification for registration and login

--- express_server.js
+++ express_server.js
@@ -13,6 +13,48 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
+
+const generateRandomString = () => {
+  let randomString = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < 6; i++) {
+    randomString += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return randomString;
+};
+
+const addUser = (id, email, password) => {
+  const newUser = {
+    id,
+    email,
+    password,
+  };
+  users[id] = newUser;
+  return id;
+};
+
+const findUser = (email) => {
+  for (let userId in users) {
+    const currentUser = users[userId];
+    if (currentUser.email === email) {
+      return currentUser;
+    }
+  }
+  return false;
+};
+
 app.get('/', (req, res) => {
   res.send('Hello!');
 });
@@ -26,9 +68,9 @@ app.get('/hello', (req, res) => {
 });
 
 app.get('/urls', (req, res) => {
-  let templateVars = { 
+  let templateVars = {
     urls: urlDatabase,
-    username: req.cookies["username"],  
+    user: users[req.cookies["user_id"]],
   };
   res.render("urls_index", templateVars);
 });
@@ -40,16 +82,30 @@ app.get("/u/:shortURL", (req, res) => {
 
 app.get('/urls/new', (req, res) => {
   let templateVars = {
-    username: req.cookies["username"],
-  }
+    user: users[req.cookies["user_id"]],
+  };
   res.render("urls_new", templateVars);
+});
+
+app.get('/register', (req, res) => {
+  let templateVars = {
+    user: users[req.cookies["user_id"]],
+  };
+  res.render("urls_register", templateVars);
+});
+
+app.get('/login', (req, res) => {
+  let templateVars = {
+    user: users[req.cookies["user_id"]],
+  };
+  res.render('urls_login', templateVars);
 });
 
 app.get('/urls/:shortURL', (req, res) => {
   let templateVars = {
     shortURL : req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL],
-    username: req.cookies["username"],
+    user: users[req.cookies["user_id"]],
   };
   res.render("urls_show", templateVars);
 });
@@ -62,21 +118,50 @@ app.post('/urls', (req, res) => {
 
 app.post('/urls/:shortURL/delete', (req, res) => {
   delete urlDatabase[req.params.shortURL];
-  res.redirect('/urls')
+  res.redirect('/urls');
 });
 
 app.post('/urls/:shortURL', (req, res) => {
   urlDatabase[req.params.shortURL] = req.body.longURL;
-  res.redirect(`/urls`)
+  res.redirect(`/urls`);
+});
+
+app.post('/register', (req, res) => {
+  const { email, password } = req.body;
+
+  if (email === "" || password === "") {
+    res.status(401).send('Invalid entry, try again!');
+    return;
+  }
+  
+  if (findUser(email)) {
+    res.status(401).send('That email is already in use');
+    return;
+  }
+  const id = generateRandomString();
+  addUser(id, email, password);
+
+  res.cookie('user_id', id);
+  res.redirect('/urls');
 });
 
 app.post('/login', (req, res) => {
-  res.cookie('username', req.body.username);
+  const { email, password } = req.body;
+  const user = findUser(email);
+  if (!user) {
+    res.status(403).send('No User found');
+    return;
+  }
+  if (user.password !== password) {
+    res.status(403).send('Invalid Password');
+    return;
+  }
+  res.cookie('user_id', user.id);
   res.redirect(`/urls`);
 });
 
 app.post('/logout', (req, res) => {
-  res.clearCookie('username', req.body.username);
+  res.clearCookie('user_id', req.body.password);
   res.redirect('/urls');
 });
 
@@ -84,11 +169,3 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
           
-const generateRandomString = () => {
-  let randomString = '';
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  for (let i = 0; i < 6; i++) {
-    randomString += characters.charAt(Math.floor(Math.random() * characters.length));
-  }
-  return randomString;
-};
