54c826da11bbd66ea5ac1e645e5fa624c4fdf635
Caronise <lucas.betancourt@hotmail.com>
2019-11-02 17:06:16 +0000

    Moved getUserByEmail function into helper.js and exported it

---
+++ helpers.js
@@ -0,0 +1,11 @@
+const getUserByEmail = function(email, users) {
+  for (let userId in users) {
+    const currentUser = users[userId];
+    if (currentUser.email === email) {
+      return currentUser;
+    }
+  }
+  return false;
+};
+
+module.exports = { getUserByEmail };
